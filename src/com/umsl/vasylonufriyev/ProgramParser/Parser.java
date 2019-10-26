88575edcddc5ae13305a2ba48b80f1daf5fb4fa9
Vasyl Onufriyev <projectclass41@gmail.com>
2019-10-25 22:43:07 -0500

    started building parser

--- src/com/umsl/vasylonufriyev/ProgramParser/Parser.java
+++ src/com/umsl/vasylonufriyev/ProgramParser/Parser.java
@@ -50,8 +50,12 @@ public class Parser {
     }
 
     private void nontermProgram() throws Exception {
-        nontermVars();
-        nontermBlock();
+        if (compareToken("VAR_TK") || compareToken("START_TK")) {
+            nontermVars();
+            nontermBlock();
+        } else {
+            throw exceptionBuilder("VAR_TK OR START_TK");
+        }
     }
 
     private void nontermBlock() throws Exception {
@@ -70,10 +74,150 @@ public class Parser {
     }
 
     private void nontermVars() throws Exception {
+        if (compareToken("VAR_TK")) {
+            getNextToken(); //consume vartk
+            if (compareToken("IDENTIFIER_TK")) {
+                getNextToken(); //consume identifiertk
+
+                if (compareToken("COLON_TK")) {
+                    getNextToken(); //consume colontk
+
+                    if (compareToken("NUMBER_TK")) {
+                        getNextToken(); //consume numbertk
+                        nontermVars();
+                    } else {
+                        throw exceptionBuilder("NUMBER_TK");
+                    }
+                } else {
+                    throw exceptionBuilder("COLON_TK");
+                }
+            } else {
+                throw exceptionBuilder("IDENTIFIER_TK");
+            }
+        } else {
+            return;
+        }
+    }
+
+    private void nontermExpr() throws Exception {
+        return;
+    }
+
+    private void nontermExprFactor() throws Exception {
+        return;
+    }
+
+    private void nontermA() throws Exception {
+        return;
+    }
+
+    private void nontermN() throws Exception {
+        return;
+    }
+
+    private void nontermNFactor() throws Exception {
+        return;
+    }
+
+    private void nontermM() throws Exception {
+        return;
+    }
+
+    private void nontermR() throws Exception {
         return;
     }
 
     private void nontermStats() throws Exception {
+        nontermStat();
+        if (compareToken("SEMICOLON_TK")) {
+            getNextToken();
+        } else {
+            throw exceptionBuilder("SEMICOLON_TK");
+        }
+        nontermMStat();
+        return;
+    }
+
+    private void nontermMStat() throws Exception {
+        //in, out, start, cond, iterate, Identifier
+        if (compareToken("IN_TK") ||
+                compareToken("OUT_TK") ||
+                compareToken("START_TK") ||
+                compareToken("COND_TK") ||
+                compareToken("ITERATE_TK") ||
+                compareToken("IDENTIFIER_TK")) {
+            nontermStat();
+            if (compareToken("SEMICOLON_TK")) {
+                getNextToken();
+            }
+            nontermMStat();
+        }
+        return;
+    }
+
+    private void nontermStat() throws Exception {
+        if (compareToken("IN_TK")) {
+            nontermIn();
+        } else if (compareToken("OUT_TK")) {
+            nontermOut();
+        } else if (compareToken("START_TK")) {
+            nontermBlock();
+        } else if (compareToken("COND_TK")) {
+            nontermIf();
+        } else if (compareToken("ITERATE_TK")) {
+            nontermLoop();
+        } else if (compareToken("IDENTIFIER_TK")) {
+            nontermAssign();
+        } else {
+            throw exceptionBuilder("IN_TK, OR OUT_TK OR START_TK OR COND_TK OR ITERATE_TK OR IDENTIFIER_TK");
+        }
+
+    }
+
+    private void nontermIn() throws Exception {
+        return;
+    }
+
+    private void nontermOut() throws Exception {
+        return;
+    }
+
+    private void nontermIf() throws Exception {
+        return;
+    }
+
+    private void nontermLoop() throws Exception {
+        return;
+    }
+
+    private void nontermAssign() throws Exception {
+        if (compareToken("IDENTIFIER_TK")) {
+            getNextToken(); //consume identifier
+            if (compareToken("LESSTHAN_TK")) {
+                getNextToken(); //consume less than
+                if (compareToken("LESSTHAN_TK")) {
+                    getNextToken(); //consume less than
+                    nontermExpr();
+                } else {
+                    throw exceptionBuilder("LESSTHAN_TK");
+                }
+            } else {
+                throw exceptionBuilder("LESSTHAN_TK");
+            }
+        } else {
+            throw exceptionBuilder("IDENTIFIER_TK");
+        }
+    }
+
+    private void nontermRO() throws Exception {
+        return;
+    }
+
+    private void nontermVaROFactorGT() throws Exception {
+        return;
+    }
+
+    private void nontermROFactorLT() throws Exception {
         return;
     }
 
