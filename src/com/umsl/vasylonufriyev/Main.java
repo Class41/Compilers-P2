f3dc883028e629ba22d888e4b80b3bb2b9d425c7
Vasyl Onufriyev <projectclass41@gmail.com>
2019-11-08 18:57:36 -0600

    fixed error output

--- src/com/umsl/vasylonufriyev/Main.java
+++ src/com/umsl/vasylonufriyev/Main.java
@@ -35,7 +35,7 @@ public class Main {
             System.out.println("SUCCESSFULLY PARSED");
             ProgramTreePrint.treePrintPreorder(parseResult, 0);
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
             System.exit(-1);
         }
     }
